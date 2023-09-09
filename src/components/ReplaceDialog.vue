5dac06cf832843d8f2638c5aec67b59f34f185dc
djdjz7 <djdjz7@icloud.com>
2023-09-09 11:30:20 +0800

    ANIMATION!

--- src/components/ReplaceDialog.vue
+++ src/components/ReplaceDialog.vue
@@ -59,71 +59,91 @@ defineExpose({ openDialog });
 </script>
 
 <template>
-  <div
-    v-if="isShowing"
-    md:backdrop-blur-lg
-    fixed
-    top-0
-    left-0
-    right-0
-    bottom-0
-    md:flex="~ col items-center justify-center"
-    overflow-y-scroll
-    overflow-x-clip
-  >
+  <Transition>
     <div
-      bg-light
-      rounded-4
-      absolute
+      v-if="isShowing"
+      md:backdrop-blur-lg
+      fixed
       top-0
-      bottom-0
       left-0
       right-0
-      md:relative
-      p-6
-      md:p-12
-      md:shadow-lg
-      md:w-192
+      bottom-0
+      md:flex="~ col items-center justify-center"
+      overflow-y-scroll
+      overflow-x-clip
     >
-      <h1>Replace</h1>
-      <button
-        flex="~ justify-center items-center"
-        h-10
-        w-10
-        rounded-full
-        border-none
+      <div
+        bg-light
+        rounded-4
         absolute
-        top-6
-        right-6
-        md:top-12
-        md:right-12
-        @click="isShowing = false"
+        top-0
+        bottom-0
+        left-0
+        right-0
+        md:relative
+        p-6
+        md:p-12
+        md:shadow-lg
+        md:w-192
       >
-        <span class="material-symbols-rounded"> close </span>
-      </button>
-      <h2 block truncate>{{ fileName }}</h2>
-      <span block text-gray truncate>{{ fileUrl }}</span>
+        <h1>Replace</h1>
+        <button
+          flex="~ justify-center items-center"
+          h-10
+          w-10
+          rounded-full
+          border-none
+          absolute
+          top-6
+          right-6
+          md:top-12
+          md:right-12
+          @click="isShowing = false"
+        >
+          <span class="material-symbols-rounded"> close </span>
+        </button>
+        <h2 block truncate>{{ fileName }}</h2>
+        <span block text-gray truncate>{{ fileUrl }}</span>
 
-      <h3 m-t-2>Original file content</h3>
-      <div v-if="originalFileContent.trim() != ''" h-40 overflow-scroll m-t-1>
-        <pre break-words whitespace-pre-wrap>{{ originalFileContent }}</pre>
-      </div>
-      <div v-else m-t-1>
-        <span>Empty</span>
-      </div>
+        <h3 m-t-2>Original file content</h3>
+        <div v-if="originalFileContent.trim() != ''" h-40 overflow-scroll m-t-1>
+          <pre break-words whitespace-pre-wrap>{{ originalFileContent }}</pre>
+        </div>
+        <div v-else m-t-1>
+          <span>Empty</span>
+        </div>
 
-      <input m-t-2 block type="file" accept=".txt" @change="readFile($event)" />
+        <input
+          m-t-2
+          block
+          type="file"
+          accept=".txt"
+          @change="readFile($event)"
+        />
 
-      <h3 m-t-1>New file content</h3>
-      <div v-if="newFileContent.trim() != ''" h-40 overflow-scroll m-t-1>
-        <pre break-words whitespace-pre-wrap>{{ newFileContent }}</pre>
+        <h3 m-t-1>New file content</h3>
+        <div v-if="newFileContent.trim() != ''" h-40 overflow-scroll m-t-1>
+          <pre break-words whitespace-pre-wrap>{{ newFileContent }}</pre>
+        </div>
+        <div v-else m-t-1>
+          <span>Empty</span>
+        </div>
+        <button v-if="newFileContent.trim() != ''" m-t-2 @click="beginUpload()">
+          Start
+        </button>
       </div>
-      <div v-else m-t-1>
-        <span>Empty</span>
-      </div>
-      <button v-if="newFileContent.trim() != ''" m-t-2 @click="beginUpload()">
-        Start
-      </button>
     </div>
-  </div>
+  </Transition>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 100ms ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
